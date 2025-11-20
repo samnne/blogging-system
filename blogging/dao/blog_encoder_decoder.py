90a4af8bac4746aa2bdaca2dcee4ea2d83704167
Samuel Echefu <samuelnnechefu@gmail.com>
2025-11-19 16:03:41 -0800

    succesfully passed integration test test_create_search_blog

---
+++ blogging/dao/blog_encoder_decoder.py
@@ -0,0 +1,27 @@
+from json import JSONDecoder, JSONEncoder
+
+from blogging.blog import Blog
+
+
+class BlogEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Blog):
+            return {
+                "__type__": "Blog",
+                "id": obj.id,
+                "name": obj.name,
+                "email": obj.email,
+                "url": obj.url,
+            }
+        return super().default(obj)
+
+
+class BlogDecoder(JSONDecoder):
+    def __init__(self, *args, **kwargs):
+        super().__init__(object_hook=self.object_hook, *args, **kwargs)
+
+    def object_hook(self, dct):
+        if "__type__" in dct and dct["__type__"] == "Blog":
+            return Blog(
+                id=dct["id"], name=dct["name"], email=dct["email"], url=dct["url"]
+            )
