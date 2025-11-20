21b175b6d5c05dad9caeb8ddbd276f4c0f7a4dcc
Samuel Echefu <samuelnnechefu@gmail.com>
2025-11-20 13:26:19 -0800

    removed blogs.json from origin

--- blogging/dao/post_dao.py
+++ blogging/dao/post_dao.py
@@ -36,6 +36,7 @@ class PostDAOPickle(PostDAO):
 
     def __init__(self, blog):
 
+        # Persistence variables and config
         self.autosave = Configuration.autosave
         self.blog = blog
         self.blog_records_file = (
@@ -43,19 +44,22 @@ class PostDAOPickle(PostDAO):
             + f"/{self.blog.id}"
             + Configuration.records_extension
         )
+        # the posts array
         self.posts: list[Post] = []
         if self.autosave:
 
             try:
+                # if there are posts set the counter to the maximum value
                 with open(self.blog_records_file, "rb") as file:
                     self.posts = pickle.load(file)
                     if self.posts:
                         self.blog.counter = self.posts[-1].code
             except Exception as e:
                 print(f"file error {e}")
+                # create the new file with an empty array initalized
                 pickle_update_file(self.posts, self.blog_records_file)
 
-    def search_post(self, key: int):  # type: ignore
+    def search_post(self, key: int):
         """
         DAO implementaion of search_post
         searches to find the post given current blog
@@ -64,7 +68,7 @@ class PostDAOPickle(PostDAO):
         """
         return binary_search(self.posts, key)
 
-    def create_post(self, post: Post):  # type: ignore
+    def create_post(self, post: Post):
         """
         DAO implementaion of create_post
         Args: post (Post) -> the new post to be created
@@ -74,10 +78,11 @@ class PostDAOPickle(PostDAO):
         post.code = self.blog.counter
         self.posts.append(post)
         if self.autosave:
+            # only save if autosave is true
             pickle_update_file(self.posts, self.blog_records_file)
         return post
 
-    def retrieve_posts(self, search_string: str):  # type: ignore
+    def retrieve_posts(self, search_string: str):
         """
         Retrieves all the post given a text search string
 
@@ -87,17 +92,19 @@ class PostDAOPickle(PostDAO):
         filtered_list: list[Post] = [
             post
             for post in self.posts
+            # fileter by search_string
             if search_string in post.title or search_string in post.text
         ]
 
         return filtered_list
 
-    def update_post(self, key: int, new_title: str, new_text: str):  # type: ignore
+    def update_post(self, key: int, new_title: str, new_text: str):
 
         post = self.search_post(key)
         if post:
             post.set_values(new_title, new_text)
             if self.autosave:
+                # only save if autosave is true
                 pickle_update_file(self.posts, self.blog_records_file)
             return post
         return None
@@ -114,6 +121,7 @@ class PostDAOPickle(PostDAO):
         self.posts = [post for post in self.posts if post.code != key]
 
         if self.autosave:
+            # only save if autosave is true
             pickle_update_file(self.posts, self.blog_records_file)
         return True
 
